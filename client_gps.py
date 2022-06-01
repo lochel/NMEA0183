b4fcee4269ccc49afd1964465f478a14b074b697
Lennart Ochel <lennart.ochel@ri.se>
2022-06-01 22:29:52 +0200

    New version

--- client_gps.py
+++ client_gps.py
@@ -13,67 +13,79 @@ from serial.serialutil import EIGHTBITS, PARITY_NONE, STOPBITS_ONE
 import NMEA0183
 
 
-def main(port: int, status: bool):
-  with serial.Serial('/dev/serial0', baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE) as ser:
-    ser.readline() # trash first line
+class NMEA_GPS:
+  def __init__(self, status):
+    self._status = status
+    self._alive = True
 
-    #r = requests.get('https://sailingjackpot.ddns.net/nmea/gps?date=2022-06-01%2016:43:02&lat=N%2058%C2%B0%2026%27%2009.6%22&lon=E%20015%C2%B0%2037%27%2022.8%22')
-    #print(r.status_code)
+    self._time = None
+    self._speed = None
+    self._heading = None
+    self._latitude = None
+    self._longitude = None
+    self._altitude = None
 
-    gpx = gpxpy.gpx.GPX()
+    self.gpx = None
+
+  def new_file(self):
+    if self.gpx:
+      with open(self.filename, 'w') as f:
+        f.write(self.gpx.to_xml())
+        r = requests.get(f'https://sailingjackpot.ddns.net/nmea/gps?date={self._time}&lat={self._latitude}&lon={self._longitude}')
+        if self._status:
+          logging.info(f'request status: {r.status_code}')
+
+    self.filename = f'{self._time}.gpx'
+    if self._status:
+      logging.info(f'New file started to record: {self.filename}')
+
+    self.gpx = gpxpy.gpx.GPX()
 
     # Create first track in our GPX:
-    gpx_track = gpxpy.gpx.GPXTrack()
-    gpx.tracks.append(gpx_track)
+    self.gpx_track = gpxpy.gpx.GPXTrack()
+    self.gpx.tracks.append(self.gpx_track)
 
     # Create first segment in our GPX track:
-    gpx_segment = gpxpy.gpx.GPXTrackSegment()
-    gpx_track.segments.append(gpx_segment)
+    self.gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    self.gpx_track.segments.append(self.gpx_segment)
 
-    count = 0
-    index = 0
+  def update(self):
+    if not self.gpx:
+      self.new_file()
 
-    while True:
-      try:
-        sentence = NMEA0183.bytes_to_sentence(ser.readline())
-      except Exception:
-        pass
-      except KeyboardInterrupt:
-        with open("output.gpx", "w") as f:
-          f.write(gpx.to_xml())
+    self.gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(self._latitude, self._longitude, elevation=self._altitude, time=self._time))
+    if len(self.gpx_segment.points) >= 60*15:
+      self.new_file()
 
-        gpx_segment.simplify(100)
-        with open("output_simplified.gpx", "w") as f:
-          f.write(gpx.to_xml())
-        sys.exit()
-      else:
-        if sentence.topic == b'RMC':
-          count = count+1
-          rmc = NMEA0183.RMC(sentence)
-          #r = requests.get(f'https://sailingjackpot.ddns.net/nmea/gps?date={rmc.time}&lat={rmc.latitude}&lon={rmc.longitude}')
-          #print(r.status_code)
-
-          # Create points:
-          gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(rmc.latitude, rmc.longitude, elevation=1234, time=rmc.time))
-        if status:
-          logging.info(sentence.topic)
-
-        if count > 60*5:
-          count = 0
-          with open(f"output_{index}.gpx", "w") as f:
-            f.write(gpx.to_xml())
-
-          gpx_segment.simplify(100)
-          with open(f"output_{index}_simplified.gpx", "w") as f:
-            f.write(gpx.to_xml())
-          index = index + 1
+  def main(self):
+    with serial.Serial('/dev/serial0', baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE) as ser:
+      ser.readline() # trash first line
+      while self._alive:
+        try:
+          sentence = NMEA0183.bytes_to_sentence(ser.readline())
+          if self._status:
+            logging.info(sentence)
+        except Exception as e:
+          if self._status:
+            logging.error(e)
+        except KeyboardInterrupt:
+          self._alive = False
+        else:
+          if sentence.topic == b'RMC':
+            rmc = NMEA0183.RMC(sentence)
+            self._time = rmc.time
+            self._speed = rmc.speed
+            self._heading = rmc.heading
+            self._latitude = rmc.latitude
+            self._longitude = rmc.longitude
+            self.update()
 
 if __name__ == '__main__':
   logging.basicConfig(level=logging.INFO, format='%(asctime)s: %(levelname)s [%(name)s] %(message)s')
 
   PARSER = argparse.ArgumentParser(description='NMEA0183 GPS client', allow_abbrev=False, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-  PARSER.add_argument('--port', type=int, default=5556, help='port')
   PARSER.add_argument('--status', type=bool, default=False, help='Show status information')
   ARGS = PARSER.parse_args()
 
-  main(ARGS.port, ARGS.status)
+  gps_client = NMEA_GPS(ARGS.status)
+  gps_client.main()
